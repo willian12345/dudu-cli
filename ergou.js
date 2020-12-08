f9af3c7d19d84f1abb7da90b9a0b5becc29666c8
sheldon <willian12345@126.com>
2020-12-08 15:00:09 +0800

    fea: command line could create new project

--- ergou.js
+++ ergou.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const inquirer = require('inquirer');
 const ora = require('ora')
 const spinner = ora('Loading')
 const fs = require('fs')
@@ -13,11 +14,40 @@ const zipName = 'proejct.zip'
 const unzipedDirName = 'vue-multipage-master'
 const tempDownloadDir = './.downloads/'
 
+
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: '项目名称',
+    default: 'helloworld'
+  },
+  {
+    type: 'input',
+    name: 'version',
+    message: '版本',
+    default: '1.0.0'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: '项目描述',
+    default: ''
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: '作者',
+    default: 'alien'
+  }
+]
+
+
 function deleteFolderRecursive(path) {
   if( fs.existsSync(path) ) {
       fs.readdirSync(path).forEach(function(file) {
           var curPath = path + "/" + file;
-          if(fs.statSync(curPath).isDirectory()) { // recurse
+          if(fs.statSync(curPath).isDirectory()) {
               deleteFolderRecursive(curPath);
           } else { // delete file
               fs.unlinkSync(curPath);
@@ -26,33 +56,14 @@ function deleteFolderRecursive(path) {
       fs.rmdirSync(path);
   }
 };
-// function rmdir(dir){
-  
-//   const files = fs.readdirSync(dir)
-//   function next(index){
-//     console.log(index, files.length)
-//     if(index === files.length) {
-//       return fs.rmdirSync(dir)
-//     }
-//     let newPath = path.join(dir, files[index])
-//     fs.stat(newPath, (err, stat) => {
-//       if(stat.isDirectory()){
-//         rmdir(newPath, () => { next(index+1) })
-//       }else{
-//         fs.unlink(newPath, () => { next(index+1) })
-//       }
-//     })
-//   }
-//   next(0)
-// }
+
 
 /**
  * 下载多页应用模板并解压
  */
-const downloadTemplate = async function({version, projectName}) {
+const downloadTemplate = async function(answers) {
   spinner.start()
   spinner.color = 'green';
-
   const downloader = new Downloader({     
     url: zipUrl,     
     directory: tempDownloadDir,
@@ -70,37 +81,43 @@ const downloadTemplate = async function({version, projectName}) {
   // 解压 zip 包
   var zip = new admZip(tempDownloadDir + zipName)
   zip.extractAllTo(".", true)
+
+  // 如果已存在项目名相同的文件夹，则删除
+  if(fs.existsSync('./' + answers.name)){
+    deleteFolderRecursive('./' + answers.name)
+  }
+  // 重命令 'vue-multipage-master' 为用户输入的 name
+  fs.renameSync(unzipedDirName, './' + answers.name)
   // 删除下载的临时目录
   deleteFolderRecursive(tempDownloadDir)
-  spinner.text = `解压完成`;
+  spinner.text = `解压完成`
   spinner.succeed()
-  
   // 修改解压后的项目文件
-  editFile({version, projectName})
+  editFile(answers)
 }
 
 /**
  * 修改 package.json 文件以便修改
  */
- const editFile = function({ version, projectName }) {
-  const data = fs.readFileSync(`${process.cwd()}/${unzipedDirName}/package.json`)
+ const editFile = function({ version, name, author, description }) {
+  const data = fs.readFileSync(`${process.cwd()}/${name}/package.json`)
   let _data = JSON.parse(data.toString())
-  _data.name = projectName
+  _data.name = name
   _data.version = version
+  _data.author = author
+  _data.description = description
   let str = JSON.stringify(_data, null, 4)
   // 写入文件
-  fs.writeFileSync(`${process.cwd()}/${unzipedDirName}/package.json`, str)
+  fs.writeFileSync(`${process.cwd()}/${name}/package.json`, str)
 };
 
+// 接受命令行输入
 program
-  .version('0.0.1', '-v, --version')
-
-// 初始化命令
-program
-  .command('init <name> [otherOption...]')
-  .action((name, otherOption) => {
-    console.log('开始创建%s', name)
-    downloadTemplate({version: '0.0.1', projectName: name})
+  .version('v0.0.1', '-v, --version')
+  .command('init')
+  .action( async () => {
+    const answers = await inquirer.prompt(questions);
+    downloadTemplate(answers)
   });
 
 program.parse(process.argv)
--- readme.md
+++ readme.md
@@ -1,6 +1,35 @@
 ## dudu-cli
+创建 vue 多页应用
+
+vue multipage
+
+多页中使用各常用组件demo
+- createjs 使用 createjs 库 
+
+- fullpage 使用 fullpage 库
+
+- index 默认页
+
+- jsx 使用 jsx 语法
+
+- normal 普通页面
+
+- spa 多页中的单页应用
+
+- video 使用腾讯视频播放器
+
+- virtual-scroll 使用虚拟滚动列表
+
 ```
 $ npm install dudu-cli -g
-$ dudu -v
-dudu-cli version 0.9.1
+$ dudu-cli -v
+dudu-cli version 0.0.1
+```
+
+安装并启动 helloworld 新项目
+```
+$ dudu-cli init
+$ cd helloworld
+$ yarn
+$ yarn serve
 ```
